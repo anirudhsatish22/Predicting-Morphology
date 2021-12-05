b0c455801425da0550a486f1ce39890cadeca88f
anirudhsatish22 <asatish@hmc.edu>
2021-12-04 16:31:39 -0800

    added arg parser to the python file

--- reformat-data.py
+++ reformat-data.py
@@ -1,37 +1,39 @@
-# fp = open('all/spanish-train-high', 'r')
-
-# writer = open('spanish-formatted-data', 'w')
-
-
-# for line in fp:
-#     # print(line)
-#     newLine  = line.rstrip()
-    
-#     listOfWords = newLine.split("\t")
-#     lemma = listOfWords[0]
-#     features = listOfWords[2]
-    
-#     newList = [features+lemma+features, listOfWords[1]]
-#     writer.write("\t".join(newList))
-#     writer.write("\n")
+import argparse
+import sys
 
 
+def main(args):
 
-fp = open('all/spanish-test', 'r')
+    inputFp = args.input_file
 
-writer = open('spanish-test-data', 'w')
+    outputFp = args.output_file
+
+    for line in inputFp:
+        newLine  = line.rstrip()
+        
+        listOfWords = newLine.split("\t")
+        lemma = listOfWords[0]
+        conjugated = listOfWords[1]
+        features = listOfWords[2]
+        
+        if (args.train):
+            reformatted = features+lemma+features + "\t" + conjugated + "\n"
+        else:
+            reformatted = features+lemma+features + "\n"
+        outputFp.write(reformatted)
 
 
-for line in fp:
-    # print(line)
-    newLine  = line.rstrip()
-    
-    listOfWords = newLine.split("\t")
-    lemma = listOfWords[0]
-    features = listOfWords[2]
-    
-    newList = [features+lemma+features]
-    writer.write("\t".join(newList))
-    writer.write("\n")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", "-i", type=argparse.FileType('r'), help="input instances (train or test)", metavar="FILE")
+    parser.add_argument("--output_file", "-o", type=argparse.FileType('w'), help="output formatted data", metavar="FILE", default=sys.stdout)
+
+    eval_group = parser.add_mutually_exclusive_group(required=True)
+    eval_group.add_argument("--train", action="store_true")
+    eval_group.add_argument("--test", action="store_true")
 
 
+    args=parser.parse_args()
+    main(args)
+
+    for fp in (args.output_file, args.input_file): fp.close()
